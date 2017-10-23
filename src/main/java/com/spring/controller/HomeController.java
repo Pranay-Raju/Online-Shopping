1009fe9619b3eab93d5019b7c3fbca91635e868f
Pranay-Raju <Pranay-Raju@users.noreply.github.com>
2017-10-23 11:23:03 +0530

    Initial Setup

---
+++ src/main/java/com/spring/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView home(Model model){
+		ModelAndView mv = new ModelAndView("hello");
+		return mv;
+	}
+
+}
